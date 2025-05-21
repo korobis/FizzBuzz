dd65d7f1922d5ad001ab8a1afe26a92ae1bb2258
korobis <34368521+korobis@users.noreply.github.com>
2025-05-21 07:24:12 -0400

    added FizzBuzz.js

---
+++ FizzBuzz.js
@@ -0,0 +1,17 @@
+for(let i = 0; i <= 100; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBuzz")
+        continue;
+    }
+    else if(i % 3 == 0){
+        console.log("Fizz")
+        continue;
+    }
+    else if(i % 5 == 0){
+        console.log("Buzz")
+        continue;
+    }
+    else{
+        console.log(i)
+    }
+}
